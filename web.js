fd91356f1bb0f12e80a28807960a60e7fb24ffc6
Rafael da Cunha <rafa_mazza@hotmail.com>
2013-07-31 17:21:18 +0000

    index.html

--- web.js
+++ web.js
@@ -3,8 +3,16 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+var fs = require('fs');
+var infile =  "index.html";
+var text = "";
+fs.read(infile, function read(err, data) {
+  if (err) throw err;
+  text = data.toString();
+  })
+  
+response.send(text);
+}
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
